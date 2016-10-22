b5debbe8568eb15f88f86202816b37e8a24f397d
jd2rogers2 <jd2rogers2@gmail.com>
2016-10-22 16:45:11 -0400

    refactoring makeTagShowPage()

--- app/assets/javascripts/tags/show.js
+++ app/assets/javascripts/tags/show.js
@@ -32,26 +32,26 @@ class Dilemma {
 function makeTagShowPage(){
   var tag_id = $('#tag_show_page').attr('data-id');
   $.get("/tags/"+ tag_id + ".json", function(data){
-    debugger;
     var tag = new Tag(data.id, data.name, data.dilemmas);
+    debugger;
 
     // unrefactored code below still has tag as response data
-    // var dilemmas = tag.dilemmas;
-    // var h3 = `<h3>${tag.name}</h3>`;
-    // var rows = "";
-    // for (var count = 0; count < dilemmas.length; count++) {
-    //   var dilemma = dilemmas[count];
-    //   var dilemmas_tags = dilemma.tags;
-    //   var tags_a_tags = "";
-    //   for (var i = 0; i < dilemmas_tags.length; i++){
-    //     var tag = dilemmas_tags[i];
-    //     tags_a_tags += `<a href="/tags/${tag.id}">${tag.name}</a> `;
-    //   }
-    //   rows += `<tr><td><a href="/dilemmas/${dilemmas[count].id}">${dilemmas[count].name}</a></td><td>${dilemmas[count].deadline}</td><td>${tags_a_tags}</td></tr>`;
-    // }
-    // var table = `<table><tbody><tr><th>dilemma</th><th>deadline</th><th>tags</th></tr>${rows}</tbody></table>`;
-    // var total = h3 + table;
-    // $('#tag_show_page').html(total);
+    var h3 = `<h3>${tag.name}</h3>`;
+    var rows = "";
+    for (var count = 0; count < tag.dilemmas.length; count++) {
+      var dilemma = tag.dilemmas[count];
+      var dilemmas_tags = dilemma.tags;
+      var tags_a_tags = "";
+      for (var i = 0; i < dilemmas_tags.length; i++){
+        var tag = dilemmas_tags[i];
+        tags_a_tags += `<a href="/tags/${tag.id}">${tag.name}</a> `;
+      }
+      debugger; // tag does not have dilemmas attr here?????
+      rows += `<tr><td><a href="/dilemmas/${tag.dilemmas[count].id}">${tag.dilemmas[count].name}</a></td><td>${tag.dilemmas[count].deadline}</td><td>${tags_a_tags}</td></tr>`;
+    }
+    var table = `<table><tbody><tr><th>dilemma</th><th>deadline</th><th>tags</th></tr>${rows}</tbody></table>`;
+    var total = h3 + table;
+    $('#tag_show_page').html(total);
   });
 }
 
