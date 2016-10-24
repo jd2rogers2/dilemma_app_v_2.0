255d619c9c8a892abccce5b5a50ffd4a63398816
jd2rogers2 <jd2rogers2@gmail.com>
2016-10-24 12:08:07 -0400

    refactored addComment() in dilemma show.js

--- app/assets/javascripts/dilemmas/show.js
+++ app/assets/javascripts/dilemmas/show.js
@@ -13,56 +13,59 @@ Comment.allInstances = [];
 function getAndDisplayComments(){
   var dilemma_id = $('#comment_table').attr('data-dilemma-id');
   $.get("/dilemmas/" + dilemma_id + ".json", function(data){
-    buildDilemmasComments(data.comments, data.id);
-    $('#comment_header_row').after(commentsHtml());
+    buildAllComments(data.comments);
+    $('#comment_header_row').after(thisDilemmasCommentsHtml(dilemma_id));
   });
 }
 
-function buildDilemmasComments(comments, this_dilemmas_id){
+function buildAllComments(comments){
   for (var count = 0; count < comments.length; count++){
-    if (comments[count].dilemma_id === this_dilemmas_id){
-      var comment = new Comment(comments[count].id, comments[count].content, comments[count].commenter, comments[count].dilemma_id);
-    }
+    var comment = new Comment(comments[count].id, comments[count].content, comments[count].commenter, comments[count].dilemma_id);
   }
 }
 
-function commentsHtml(){
+function thisDilemmasCommentsHtml(this_dilemmas_id){
   var author_id = $('#comment_table').attr('data-author-id');
   var current_user_id = $('#comment_table').attr('data-current-user-id');
   var html = "";
   for (var count = 0; count < Comment.allInstances.length; count++){
-    var comment = Comment.allInstances[count]
-    html += "<tr><td>" + comment.commenter.email + "</td><td>" + comment.content;
-    if (author_id === current_user_id) {
-      html += " <a rel='nofollow' data-method='delete' href='/comments/" + comment.id + "'>delete</a>"
+    var comment = Comment.allInstances[count];
+    if (comment.dilemma_id == this_dilemmas_id){
+      html += "<tr><td>" + comment.commenter.email + "</td><td>" + comment.content;
+      if (author_id === current_user_id) {
+        html += " <a rel='nofollow' data-method='delete' href='/comments/" + comment.id + "'>delete</a>"
+      }
+      html += "</td></tr>";
     }
-    html += "</td></tr>";
   }
   return html;
 }
 
-function addComment(){
+function commentHtml(comment){
   var author_id = $('#comment_table').attr('data-author-id');
   var current_user_id = $('#comment_table').attr('data-current-user-id');
+  var comment_html = "<tr><td>" + comment.commenter.email + "</td><td>" + comment.content;
+  if (author_id === current_user_id) {
+    comment_html += " <a rel='nofollow' data-method='delete' href='/comments/" + comment.id + "'>delete</a>"
+  }
+  comment_html += "</td></tr>";
+}
+
+function addComment(){
   $('input#new_comment').click(function(event){
     event.preventDefault();
+    var current_user_id = $('#comment_table').attr('data-current-user-id');
     var dilemma_id = $('#comment_dilemma_id').attr('value');
-    var commenter_id = $('#comment_commenter_id').attr('value');
     var content = $('textarea').val();
-    var new_comment = {"comment" : {"content" : content, "dilemma_id" : dilemma_id, "commenter_id" : commenter_id}};
+    var new_comment = {"comment" : {"content" : content, "dilemma_id" : dilemma_id, "commenter_id" : current_user_id}};
     $.ajax({
       "url" : "/comments",
       "type" : "POST",
       "data" : new_comment,
-      "datatype" : "json",
+      "dataType" : "json",
       success : function(data) {
         var comObj = new Comment(data.id, data.content, data.commenter, data.dilemma_id)
-        var comment_html = "<tr><td>" + data.commenter.email + "</td><td>" + data.content;
-        if (author_id === current_user_id) {
-          comment_html += " <a rel='nofollow' data-method='delete' href='/comments/" + comObj.id + "'>delete</a>"
-        }
-        comment_html += "</td></tr>";
-        $('#comment_create_row').before(comment_html)
+        $('#comment_create_row').before(commentHtml(comObj))
       }, 
       error : function(data, error, message) {
         alert(message);
@@ -84,5 +87,5 @@ function showDilemma(){
 
 $(document).ready(function(){
   addComment();
-  deleteComment();
+  // deleteComment();
 });
